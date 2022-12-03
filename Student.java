68ca56cab50eecd94b96fdbd0de73b917fc22ac3
YadavalliSumaBindu <sumabindu2002@gmail.com>
2022-12-03 14:58:43 +0530

    first coomit

---
+++ Student.java
@@ -0,0 +1,26 @@
+class Student
+{
+    String name;
+    int rollno;
+    String course;
+    String email;
+    long mobno;
+    String address;
+    char gender;
+    int yr_of_joined;
+    public void printDetails()
+    {
+        System.out.println("Student Details .......");
+        System.out.println("Name :"+name);
+        System.out.println("Rollno :"+rollno);
+        System.out.println("course :"+course);
+        System.out.println("Email :"+email);
+        System.out.println("Mobile_number :"+mobno);
+        System.out.println("Address :"+address);
+        System.out.println("Gender :"+gender);
+        System.out.println("Year_of_joining :"+yr_of_joined);
+
+    }
+    
+
+}
---
+++ StudentManager.java
@@ -0,0 +1,16 @@
+class StudentManager{
+ public static void main(String[] args)
+ {
+    Student s=new Student();
+    s.name="sai";
+    s.rollno=101;
+    s.course="java";
+    s.email="sai@gmail.com";
+    s.mobno=934637234;
+    s.address="hyd";
+    s.gender='M';
+    s.yr_of_joined=2020;
+    s.printDetails();
+ }
+
+}
